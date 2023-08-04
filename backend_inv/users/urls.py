be12931351ae29ede8014e59ef7dd0104f3d9902
twanpluy <twanpluy@gmail.com>
2023-08-04 16:12:58 +0200

    adding urls path to main

--- backend_inv/users/urls.py
+++ backend_inv/users/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import UserDetailAPI, RegisterUserAPIView
 
 urlpatterns = [
-    path("register", RegisterView.as_view(), name="register"),
-    path("login", RegisterView.as_view(), name="login"),
-    path("logout", RegisterView.as_view(), name="logout"),
-    path("password", RegisterView.as_view(), name="password"),
-    path("reset-password", RegisterView.as_view(), name="reset-password"),
+    path('user_details', UserDetailAPI.as_view()),
+    path('register', RegisterUserAPIView.as_view()),
 ]
